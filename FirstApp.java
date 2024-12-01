d321f4f90cf9054cf27515200ffdd17a0fc3aaf5
Mayur Akkar <91078024+Mayurakkar10@users.noreply.github.com>
2024-12-01 15:30:20 +0530

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class FirstApp{
    public static void main(String[] args){
         System.out.println("\nFirstApp");
+        System.out.println("\nI am Developer B and I made this change");
    }
 }
